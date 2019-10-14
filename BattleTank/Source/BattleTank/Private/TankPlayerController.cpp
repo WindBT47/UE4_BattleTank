c217f2c38a1bbdefa134c15dda41ab1c3b58e31c
WindBT <zk17628037245@gmail.com>
2019-10-14 21:23:26 +0800

    2019/10/14_21:22_GetController&TankINC++Class

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+void  ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController Processing not success"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController Processing: %s"), *(ControlledTank->GetName()));
+	}
+	UE_LOG(LogTemp, Warning, TEXT("TankPlayerController BeginPlay"))
+}
+ATank* ATankPlayerController::GetControlledTank() const {
+	return Cast<ATank>(GetPawn());
+}
+
